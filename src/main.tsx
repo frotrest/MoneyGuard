9c07207b9ef1eeff610a68e34df9f8eb40d0b732
Frotrest <125584696+frotrest@users.noreply.github.com>
2026-09-07 19:41:47 +0300

    Remove basename from BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        <BrowserRouter basename="MoneyGuard">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
