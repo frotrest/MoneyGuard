0ab76d57840a7ab01e6af0d663e11d70e044a909
Frotrest <125584696+frotrest@users.noreply.github.com>
2026-09-07 19:31:48 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        <BrowserRouter basename="Moneyguard">
+        <BrowserRouter basename="MoneyGuard">
           <App />
         </BrowserRouter>
       </PersistGate>
